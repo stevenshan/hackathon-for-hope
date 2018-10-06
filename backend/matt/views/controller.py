223aa971b166339602673c52313d818daab09339
Steven Shan <steven.shan99@gmail.com>
2018-10-06 13:26:26 -0400

    for rebecca

--- backend/matt/views/controller.py
+++ backend/matt/views/controller.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import current_app as app
 import redis
 import uuid
+import datetime
 
 views = flask.Blueprint("app", __name__)
 
@@ -14,8 +15,7 @@ def _medicine():
 
     return jsonify(values)
 
-@views.route("/steven", methods=["GET"])
-def _user():
+def _getUser():
     properties = app.redis.hgetall("steven:props")
 
     medicineNames = app.redis.keys("steven:medicine:*")
@@ -24,8 +24,45 @@ def _user():
     temp = {"medicine": medicines}    
     temp.update(properties)
 
-    return jsonify(temp)
+    return temp
 
+@views.route("/steven", methods=["GET"])
+def _user():
+    return jsonify(_getUser())
+
+@views.route("/rebecca", methods=["GET"])
+def __user():
+    user = _getUser()
+
+    medicines = user["medicine"]
+    _medicines = []
+
+    today = (datetime.datetime.today().weekday() + 1) % 7
+
+    def _map(x):
+        days = str(x["days"]).split(",")
+        if str(today) in days:
+            dt = datetime.datetime.today()
+            hours = str(x["times"]).split(",")
+            for hour in hours:
+                dt = dt.replace(minute=0, second=0,microsecond=0,hour=int(hour))
+
+                start = dt.strftime("%Y-%m-%d %H:%M:%S") 
+                dt = dt.replace(minute=10)
+                end = dt.strftime("%Y-%m-%d %H:%M:%S") 
+
+                result = {
+                    "start": start,
+                    "end": end,
+                    "title": x["name"],
+                    "summary": x["instruction"],
+                }
+
+                _medicines.append(result)
+
+    map(_map, medicines)
+
+    return jsonify(_medicines)
 
 @views.route("/steven/addprescription", methods=["POST"])
 def _add():
