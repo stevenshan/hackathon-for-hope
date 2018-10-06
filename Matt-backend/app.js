946141d51d8fe9b990e23e963e95fad352d9b4fd
manu183 <manuellang183@gmail.com>
2018-10-06 06:55:43 -0400

    adding and deleting prescriptions

--- Matt-backend/app.js
+++ Matt-backend/app.js
@@ -20,13 +20,29 @@ MongoClient.connect(process.env.MONGO_URL, {
 });
 
 app.get('/', function (req, res, next) {
-    res.send('Hello Brandon');
+    res.send('<img style="width:100%;" src="https://media1.tenor.com/images/a7eda1aa3541cadbb98dcb4f57931a58/tenor.gif"/>');
+});
+
+app.get('/patients', function (req, res, next) {
+    MongoClient.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true
+    }, (err, db) => {
+        if (err) {
+            return console.log(err);
+        }
+        var dbo = db.db("mattdb");
+        dbo.collection("patients").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            db.close();
+            res.send(result);
+        });
+    });
 });
 
 app.get('/patients/:lastname/:firstname', function (req, res, next) {
-    var patient;
     var first_name = req.params.firstname;
-    var last_name = req.params.lastname;    MongoClient.connect(process.env.MONGO_URL, {
+    var last_name = req.params.lastname;
+    MongoClient.connect(process.env.MONGO_URL, {
         useNewUrlParser: true
     }, (err, db) => {
         if (err) {
@@ -38,21 +54,83 @@ app.get('/patients/:lastname/:firstname', function (req, res, next) {
             lastname: last_name
         }, function (err, result) {
             if (err) return console.log(err);
-            let med = result.medicine + '';
             let patient = result;
-            patient.medicine = [];
-            async.each(med.split(","), function(med, callback) {
-                dbo.collection("medicine").findOne({
-                    name: med
-                }, function(err,med) {
-                    if (med) 
-                        patient.medicine.push(med);
-                    callback(err);
-                });
-            }, function(err) {
-                db.close();
-                res.send(patient); 
-            });         
+            db.close();
+            res.send(patient);
+        });
+    });
+});
+
+app.post('/patients/:lastname/:firstname/addprescription', function (req, res, next) {
+    console.log(req.body);
+    var first_name = req.params.firstname;
+    var last_name = req.params.lastname;
+    MongoClient.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true
+    }, (err, db) => {
+        if (err) {
+            return console.log(err);
+        }
+        var dbo = db.db("mattdb");
+        dbo.collection("patients").findOne({
+            firstname: first_name,
+            lastname: last_name
+        }, function (err, patient) {
+            if (err) return console.log(err);
+            let medicine = {
+                "name": req.body.name,
+                "instruction": req.body.instruction,
+                "recommendation": req.body.recommendation,
+                "days": req.body.days,
+                "times": req.body.times,
+                "dosage": req.body.dosage
+            };
+            patient.medicine.push(medicine);
+            dbo.collection("patients").updateOne(
+                { firstname: first_name,
+                    lastname: last_name },
+                { $set: { "medicine" : patient.medicine } }
+             );
+            db.close();
+            res.send("Done.");
+        });
+    });
+});
+
+app.post('/patients/:lastname/:firstname/deleteprescription', function (req, res, next) {
+    console.log(req.body);
+    var first_name = req.params.firstname;
+    var last_name = req.params.lastname;
+    MongoClient.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true
+    }, (err, db) => {
+        if (err) {
+            return console.log(err);
+        }
+        var dbo = db.db("mattdb");
+        dbo.collection("patients").findOne({
+            firstname: first_name,
+            lastname: last_name
+        }, function (err, patient) {
+            if (err) return console.log(err);
+            let medicine = {
+                "name": req.body.name,
+                "instruction": req.body.instruction,
+                "recommendation": req.body.recommendation,
+                "days": req.body.days,
+                "times": req.body.times,
+                "dosage": req.body.dosage
+            };
+            console.log(medicine);
+            patient.medicine.pop(medicine);
+            dbo.collection("patients").updateOne(
+                { firstname: first_name,
+                    lastname: last_name },
+                { $set: { "medicine" : patient.medicine } }
+             );
+            console.log(patient);
+            db.close();
+            res.send("Done.");
         });
     });
 });
@@ -61,13 +139,21 @@ app.post('/patients/add', function (req, res, next) {
     var first_name = req.body.firstname;
     var last_name = req.body.lastname;
     var date_of_birth = req.body.dateofbirth;
-    var medicine = req.body.medicine;
+    var medicine_name = req.body.medicinename;
+    var medicine_days = req.body.medicinedays;
+    var medicine_times = req.body.medicinetimes;
+    var medicine_dosage = req.body.medicinedosage;
     var recommendations = req.body.recommendations;
     var patient = {
         "firstname": first_name,
         "lastname": last_name,
         "dateofbirth": date_of_birth,
-        "medicine": medicine,
+        "medicine": {
+            "name": medicine_name,
+            "days": medicine_days,
+            "times": medicine_times,
+            "dosage": medicine_dosage
+        },
         "recommendations": recommendations
     };
     MongoClient.connect(process.env.MONGO_URL, {
@@ -85,7 +171,7 @@ app.post('/patients/add', function (req, res, next) {
     res.send("Done");
 });
 
-app.get('/medicine', function(req, res, next) {
+app.get('/medicine', function (req, res, next) {
     MongoClient.connect(process.env.MONGO_URL, {
         useNewUrlParser: true
     }, (err, db) => {
@@ -93,7 +179,7 @@ app.get('/medicine', function(req, res, next) {
             return console.log(err);
         }
         var dbo = db.db("mattdb");
-        dbo.collection("medicine").find({}).toArray(function(err, result) {
+        dbo.collection("medicine").find({}).toArray(function (err, result) {
             if (err) throw err;
             db.close();
             res.send(result);
@@ -110,13 +196,14 @@ app.get('/medicine/:name', function (req, res, next) {
             return console.log(err);
         }
         var dbo = db.db("mattdb");
-        // dbo.collection("medicine").findOne({name: medicine}, function(err, result) {
-        //     if (err) return console.log(err);
-        //     console.log(result.name);
-        //     db.close();
-        // });
+        dbo.collection("medicine").findOne({
+            name: medicine
+        }, function (err, result) {
+            if (err) return console.log(err);
+            db.close();
+            res.send(result);
+        });
     });
-    res.send(medicine);
 });
 
 app.post('/medicine/add/', function (req, res, next) {
@@ -125,12 +212,11 @@ app.post('/medicine/add/', function (req, res, next) {
     var _recommendation = req.body.recommendation;
     var _dosage = req.body.dosage;
     var _times = req.body.times;
+    var _days = req.body.days;
     var medicine = {
         "name": _name,
         "instruction": _instruction,
-        "recommendation": _recommendation,
-        "dosage": _dosage,
-        "times": _times
+        "recommendation": _recommendation
     };
     MongoClient.connect(process.env.MONGO_URL, {
         useNewUrlParser: true
