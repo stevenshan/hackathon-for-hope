083d198a7fbd088f78ea3cd681554139889afb3d
Rebecca Finn <rebecca.finn.8@gmail.com>
2018-10-06 03:54:31 -0400

    fix camera button

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -19,29 +19,24 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
   render() {
+      //fetch('http://128.237.165.133:8000/patients/lang/manuel').then((response => dd
       const { navigate } = this.props.navigation;
       var f = `${FileSystem.documentDirectory}photos/image.jpg`;
       return (
-
           <View>
-          
-          <Image style={{width: 100, height: 100 }} source = {{uri: f}}  /> 
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-            <View style={styles.container}>
-                <TouchableHighlight onPress = {() => { console.log("asd"); navigate('Medication') }}>
-                    <View style={styles.button}>
-                    <Text>Next Medication</Text>
-                    </View>
-                </TouchableHighlight>
-            </View>
-
-        </ScrollView>
+            <Image style={{width: 100, height: 100 }} source = {{uri: f}}  /> 
+            <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+                <View style={styles.container}>
+                    <TouchableHighlight onPress = {() => { navigate('Camera') }}>
+                        <View style={styles.button}>
+                            <Text>Log Medication</Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+            </ScrollView>
           </View>
-          
-          
           );
   }
-
 }
 
 const styles = StyleSheet.create({
